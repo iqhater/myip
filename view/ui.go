2c05d1a919d93fd06710ec25ca0ff05c96c277c1
iqhater <iqhater@yandex.ru>
2025-03-10 00:09:56 +0300

    fix one vertical line fromat output.

--- view/ui.go
+++ view/ui.go
@@ -15,14 +15,14 @@ var out io.Writer = os.Stdout
 // PrintInternal print all internal ip data
 func PrintInternal(d *data.IPData) {
 	color.New(color.FgMagenta).Fprintln(out, "Local interface name:", d.AdapterName)
-	fmt.Fprintf(out, "	 Internal IP (%s): %s \n", d.InternalData.IPAddressType, d.IntIP)
+	fmt.Fprintf(out, "  Internal IP (%s): %s \n", d.InternalData.IPAddressType, d.IntIP)
 }
 
 // PrintExternal print all external ip data
 func PrintExternal(d *data.IPData) {
 
 	if d.ExtIP != "" {
-		color.New(color.FgGreen).Fprintf(out, "	 External IP (%s): %s \n", d.ExternalData.IPAddressType, d.ExtIP)
+		color.New(color.FgGreen).Fprintf(out, "  External IP (%s): %s \n", d.ExternalData.IPAddressType, d.ExtIP)
 	}
 
 	if d.Country != "" {
